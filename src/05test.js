d3f22f457b5640ab52589859bd76e041c576e32d
LiZheGuang <1030660726@qq.com>
2023-05-13 02:52:45 +0800

    feat: 🎸 super

--- src/05test.js
+++ src/05test.js
@@ -4,7 +4,7 @@
 
 const { ethers } = require("ethers");
 // glob
-const { KEY, SUPER_PASSWORD } = require("../glob.js");
+const { KEY, SUPER_PASSWORD, SUPER_PASSWORD_ONE_USER } = require("../glob.js");
 
 // key http
 const ALCHEMY_MAINNET_URL = KEY;
@@ -17,10 +17,7 @@ const main = async () => {
   // metakMask
   const wallet1 = new ethers.Wallet(privateKey, provider);
   // 随机
-  const wallet2 = new ethers.Wallet(
-    "0x79077c65cb80361462062202fce04c80de1c3ebd1033de231b9b595b59b3967c",
-    provider
-  );
+  const wallet2 = new ethers.Wallet(SUPER_PASSWORD_ONE_USER, provider);
 
   const address1 = await wallet1.getAddress();
   const address2 = await wallet2.getAddress();
@@ -34,21 +31,39 @@ const main = async () => {
   // console.log(`钱包1发送交易次数: ${txCount1}`);
   // console.log(`钱包2发送交易次数: ${txCount2}`);
 
-    const getBalance1 = await provider.getBalance(address1);
-    const getBalance2 = await provider.getBalance(address2);
-      console.log(`i. 发送前余额`);
-    console.log(`钱包1: ${ethers.formatEther(getBalance1)} ETH`);
-    console.log(`钱包2: ${ethers.formatEther(getBalance2)} ETH`);
-
+  const getBalance1 = await provider.getBalance(address1);
+  const getBalance2 = await provider.getBalance(address2);
+  console.log(`i. 发送前余额`);
+  console.log(`钱包1: ${ethers.formatEther(getBalance1)} ETH`);
+  console.log(`钱包2: ${ethers.formatEther(getBalance2)} ETH`);
+  function timeout(ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error("Timeout exceeded."));
+      }, ms);
+    });
+  }
   // 构造交易请求
   const tx = {
     to: address2,
     value: ethers.parseEther("0.001"),
   };
   console.log(`\nii. 等待交易在区块链确认（需要几分钟）`);
-  const receipt = await wallet1.sendTransaction(tx);
-  await receipt.wait(); // 等待链上确认交易
-  console.log(receipt); // 打印交易详情
+
+  // 等待 receipt 的同时，限制等待时间为 5 分钟
+  try {
+    const receipt = await Promise.race([
+      wallet1.sendTransaction(tx),
+      timeout(5 * 60 * 1000),
+    ]);
+    await receipt.wait();
+    console.log(receipt); // 打印交易详情
+  } catch (error) {
+    console.log(error);
+  }
+  //   const receipt = await wallet1.sendTransaction(tx);
+  //   await receipt.wait(); // 等待链上确认交易
+  //   console.log(receipt); // 打印交易详情
 
   // iv. 打印交易后余额
   console.log(`\niii. 发送后余额`);
